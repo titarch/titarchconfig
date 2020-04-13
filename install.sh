69fbb7fb139643793a31ecdd2aca5a321306d91d
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-04-13 17:06:41 +0200

    Add confirmation to make installations

--- install.sh
+++ install.sh
@@ -1,28 +1,54 @@
 #!/bin/sh
 
-sudo pacman -S git go gvim curl zsh reflector rofi otf-fira-code otf-fira-sans otf-fire-mono ttf-dejavu chromium feh vlc flameshot kitty rxvt-unicode
-[ $? -ne 0 ] && exit 1
-cd /tmp
-git clone https://aur.archlinux.org/yay.git
-[ $? -ne 0 ] && exit 2
-cd yay
-makepkg -si
-[ $? -ne 0 ] && exit 3
+read -p "Install base packages? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo pacman -S git go gvim curl zsh reflector rofi otf-fira-code otf-fira-sans otf-fire-mono ttf-dejavu chromium feh vlc flameshot kitty rxvt-unicode
+    [ $? -ne 0 ] && exit 1
+fi
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-[ $? -ne 0 ] && exit 4
+read -p "Install yay? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cd /tmp
+    git clone https://aur.archlinux.org/yay.git
+    [ $? -ne 0 ] && exit 2
+    cd yay
+    makepkg -si
+    [ $? -ne 0 ] && exit 3
+fi
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-[ $? -ne 0 ] && exit 5
+read -p "Install oh-my-zsh? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    [ $? -ne 0 ] && exit 4
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    [ $? -ne 0 ] && exit 5
+fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-[ $? -ne 0 ] && exit 6
+read -p "Install vundle? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    [ $? -ne 0 ] && exit 6
+fi
 
-cp xinitrc ~/.xinitrc
-cp -r i3 ~/.config
-cp bashrc .bashrc
-cp zshrc .zshrc
-cp -r dracula/* ~/.oh-my-zsh/themes
-cp Xresources ~/.Xresources
-sudo reflector --verbose --country 'France' -l 10 -p https --sort rate --save /etc/pacman.d/mirrorlist
-cp vimrc ~/.vimrc
+
+read -p "Copy config? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cp xinitrc ~/.xinitrc
+    cp -r i3 ~/.config
+    cp bashrc .bashrc
+    cp zshrc .zshrc
+    cp -r dracula/* ~/.oh-my-zsh/themes
+    cp Xresources ~/.Xresources
+    cp vimrc ~/.vimrc
+fi
+
+read -p "Refresh mirror? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo reflector --verbose --country 'France' -l 10 -p https --sort rate --save /etc/pacman.d/mirrorlist
+fi
