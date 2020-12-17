abaf8282131ca030406bf74fab2dccf9f0912423
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-12-17 10:45:30 +0100

    Add rofi config

--- install.sh
+++ install.sh
@@ -79,6 +79,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     mkdir -p ~/.config/dunst/
     ln -sf $PWD/dunstrc ~/.config/dunst/dunstrc
     ln -sf $PWD/gitconfig ~/.gitconfig
+    ln -sf $PWD/rofi ~/.config/rofi
 fi
 
 read -p "Generate ssh-key? " -n 1 -r
