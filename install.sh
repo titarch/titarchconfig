bd32f8293bd0b5d9894c9c4855453394a82db644
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-12-17 13:58:08 +0100

    Add papirus icon theme to extras

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo pacman -S go rofi otf-fira-sans otf-fira-mono ttf-dejavu chromium \
         feh vlc flameshot kitty playerctl pavucontrol thunar arandr pasystray \
         cmake dunst lxappearance pulseaudio sysstat pamixer acpi \
-        awesome-terminal-fonts ttf-font-awesome otf-font-awesome
+        awesome-terminal-fonts ttf-font-awesome otf-font-awesome papirus-icon-theme
     [ $? -ne 0 ] && exit 1
 fi
 
