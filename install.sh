9908829a057d96c436ac5efd736b42cd1e08a027
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-12-18 11:25:42 +0100

    Add new keybindings utils and update rofi config

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         feh vlc flameshot kitty playerctl pavucontrol thunar arandr pasystray \
         cmake dunst lxappearance pulseaudio sysstat pamixer acpi htop bashtop \
         awesome-terminal-fonts ttf-font-awesome otf-font-awesome iotop tig \
-        papirus-icon-theme
+        papirus-icon-theme xdotool xsel xclip rofimoji qrencode rofi-calc
     [ $? -ne 0 ] && exit 1
 fi
 
