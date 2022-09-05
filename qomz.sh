f875f6644fbca6279d9e7b0d4674e57b415d9b0c
Baptiste Parsy <parsybaptiste@gmail.com>
2022-09-05 20:59:14 +0200

    Add quick omz install script

---
+++ qomz.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# qomz (Quick Oh My Zsh)
+set -e
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo chsh -s $(which zsh) $(id -un)
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/superbrothers/zsh-kubectl-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-kubectl-prompt
+git clone https://github.com/hanjunlee/terragrunt-oh-my-zsh-plugin ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/terragrunt
+
+curl -o "$HOME"/.zshrc -L https://raw.githubusercontent.com/titarch/titarchconfig/master/zshrc
+mkdir -p "$HOME"/.oh-my-zsh/themes/lib/
+curl -o "$HOME"/.oh-my-zsh/themes/dracula.zsh-theme https://raw.githubusercontent.com/titarch/titarchconfig/master/dracula/dracula.zsh-theme
+curl -o "$HOME"/.oh-my-zsh/themes/lib/async.zsh https://raw.githubusercontent.com/titarch/titarchconfig/master/dracula/lib/async.zsh
