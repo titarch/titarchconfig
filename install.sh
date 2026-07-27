e98780d61a76934a69cf860c86bc1033a27bfb24
Baptiste Parsy <parsybaptiste@gmail.com>
2026-07-27 10:43:58 +0200

    zsh: wire forgit under an fg- prefix (FORGIT_NO_ALIASES preserves omz git aliases)

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ read -p "Install AUR packages (cursors, folder colors)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     # -bin is the versioned greeter launcher; -git tracks master and churns
-    yay -S catppuccin-cursors-mocha papirus-folders greetd-dms-greeter-bin dracula-gtk-theme
+    yay -S catppuccin-cursors-mocha papirus-folders greetd-dms-greeter-bin dracula-gtk-theme forgit
     sudo papirus-folders -C violet --theme Papirus-Dark
 fi
 
